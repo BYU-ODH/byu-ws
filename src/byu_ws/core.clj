9f77fbca24cfa7425601f22e3f654bdf7506ac5b
Tory BYU <torys.anderson@gmail.com>
2016-04-28 16:17:59 -0600

    Got the right credential sfigured out for both

--- src/byu_ws/core.clj
+++ src/byu_ws/core.clj
@@ -13,25 +13,21 @@
 
 (defonce DATE-FORMAT (java.text.SimpleDateFormat. "YYYY-MM-dd HH:mm:ss"))
 
-(defonce SERVICE-URLS {:records "https://api.byu.edu/rest/v1/apikey/academic/records/studentrecord/"
-                       :schedule "https://ws.byu.edu/rest/v1.0/academic/registration/studentschedule/"})
+(defonce SERVICE-URLS {:records "https://api.byu.edu/rest/v1/apikey/academic/records/studentrecord/" ;; requires "grants" credentials
+                       :schedule "https://ws.byu.edu/rest/v1.0/academic/registration/studentschedule/" ;; requires "humanities" credentials
+                       })
 
 (defn make-sha512-hmac [shared-secret item-to-encode]
-  ;; base code from Node.js
   ;; better instructions: https://byuapi.atlassian.net/wiki/display/OITCoreDeveloperResources/Web+Service+using+Nonce-Encoded+HMAC+Signed+by+an+API+Key+Tutorial
   (let [algorithm "HmacSHA512"
         key-spec (SecretKeySpec. (.getBytes shared-secret "UTF8") algorithm)
         mac (Mac/getInstance (.getAlgorithm key-spec))
         _ (.init mac key-spec)
-        encoder (new org.apache.commons.codec.binary.Base64 0)
-        ]
+        encoder (new org.apache.commons.codec.binary.Base64 0)]
     (-> item-to-encode
         (.getBytes "UTF8")
         (->> (.doFinal mac)
-             (.encodeToString encoder)))
-    ;; (->> item-to-encode .getBytes (.doFinal mac) Hex/encodeHexString
-    ;;      ) ;; ?? Might need to be base 64 directly, not hexString first
-    ))
+             (.encodeToString encoder)))))
 
 (defn url-encode ;; XX TODO not fully compatible with make-sha512-hmac
   "Encode for URL validation"
@@ -60,9 +56,7 @@
                               end-str)
                          
                          :default (str request-body end-str))] ;; when no request-body]
-    ;; return val
-    (->> item-to-encode (make-sha512-hmac shared-secret) ;; remember: changing the var order from the js
-         r/base64-encode)))
+    (make-sha512-hmac shared-secret item-to-encode )))
 
 (defn get-nonce [api-key actor]
   (let [actor (if (empty? actor)
@@ -74,9 +68,6 @@
 
 (defn nonce-encode [shared-secret nonce-value]
   (make-sha512-hmac shared-secret nonce-value))
-  ;; (-> (make-sha512-hmac shared-secret nonce-value)
-  ;;     (.getBytes)
-  ;;     r/base64-encode))
 
 (defn get-http-authorization-header
   "Get the authorization header necessary for use of some BYU web services"
@@ -91,8 +82,7 @@
               http-method
               actor-in-hash
               current-timestamp]
-       :or { ;; Grants or humanities keys? We'll start with grants. 
-            api-key "Ovo1FxW0yAz7-HNbdnM9"
+       :or {api-key "Ovo1FxW0yAz7-HNbdnM9"
             shared-secret "hziQkSgRpdZla_giFfCK4h_OT98ykZM4ZYfoERvB"
             key-type "API"
             encoding-type "Nonce"
@@ -116,15 +106,15 @@
     (throw (Exception. (str "Invalid key-type " key-type)))))
 
 
-
+;; (get-student-data {:service :schedule :param (str personid "/20121")}) for schedule, with the 
 (defn get-student-data [&[{:keys [service param netid]
                            :or {service :records
-                                param "081270232" ;; TODO, IS NOT THE BYU ID#. get from person info (cas login?)
+                                param "081270232" ;; TODO, for records this is personid, for schedule this is personid/yearterm
                                 netid "torysa" ;; TODO
                                 }}]]
-  (let [service-url (str (SERVICE-URLS service) param) ;; year necessary?
-        auth-header (get-http-authorization-header {; :api-key ;TODO get from env?
-                                        ; :shared-secret ;TODO get from env?
+  (let [service-url (str (SERVICE-URLS service) param)
+        auth-header (get-http-authorization-header {:api-key "0e4KkLXo6NgilKScIjh4" ;; TODO this is the "humanities" one for the schedule
+                                                    :shared-secret "4mGgi8E1a-bGd-4rMWOgsS_2-S33i104JHHDIiYp" ;; TODO this is the "humanities" one for the schedule
                                                     :key-type "API" ;; Unnecessary
                                                     :encoding-type "Nonce"
                                                     :url service-url
