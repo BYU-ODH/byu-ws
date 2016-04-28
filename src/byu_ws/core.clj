838b553b43ebba7f57e548a2116cf3cb2c9f836e
Tory BYU <torys.anderson@gmail.com>
2016-04-28 15:13:52 -0600

    Got the web service call working

--- src/byu_ws/core.clj
+++ src/byu_ws/core.clj
@@ -18,17 +18,22 @@
 
 (defn make-sha512-hmac [shared-secret item-to-encode]
   ;; base code from Node.js
-  ;; var hash = crypto.createHmac('sha512',sharedSecret);
-  ;; hash.update(string);
-  ;; return hash.digest();
+  ;; better instructions: https://byuapi.atlassian.net/wiki/display/OITCoreDeveloperResources/Web+Service+using+Nonce-Encoded+HMAC+Signed+by+an+API+Key+Tutorial
   (let [algorithm "HmacSHA512"
-        key-spec (SecretKeySpec. (.getBytes shared-secret) algorithm)
-        mac (Mac/getInstance algorithm)
-        _ (.init mac key-spec)]
-    (->> item-to-encode .getBytes (.doFinal mac) Hex/encodeHexString) ;; ?? Might need to be base 64 directly, not hexString first
+        key-spec (SecretKeySpec. (.getBytes shared-secret "UTF8") algorithm)
+        mac (Mac/getInstance (.getAlgorithm key-spec))
+        _ (.init mac key-spec)
+        encoder (new org.apache.commons.codec.binary.Base64 0)
+        ]
+    (-> item-to-encode
+        (.getBytes "UTF8")
+        (->> (.doFinal mac)
+             (.encodeToString encoder)))
+    ;; (->> item-to-encode .getBytes (.doFinal mac) Hex/encodeHexString
+    ;;      ) ;; ?? Might need to be base 64 directly, not hexString first
     ))
 
-(defn url-encode
+(defn url-encode ;; XX TODO not fully compatible with make-sha512-hmac
   "Encode for URL validation"
   [[{:keys [shared-secret
             current-timestamp
@@ -51,13 +56,13 @@
                          (str (clojure.string/upper-case http-method) "\n"
                               host "\n"
                               request-uri "\n"
-                              ;(sort-params request-body) ;; TODO sort-params? Why?
+                                        ;(sort-params request-body) ;; TODO sort-params? Why?
                               end-str)
                          
                          :default (str request-body end-str))] ;; when no request-body]
     ;; return val
     (->> item-to-encode (make-sha512-hmac shared-secret) ;; remember: changing the var order from the js
-        r/base64-encode)))
+         r/base64-encode)))
 
 (defn get-nonce [api-key actor]
   (let [actor (if (empty? actor)
@@ -68,40 +73,65 @@
     (json/parse-string (:body nonce))))
 
 (defn nonce-encode [shared-secret nonce-value]
-  (-> (make-sha512-hmac shared-secret nonce-value)
-      r/base64-encode))
+  (make-sha512-hmac shared-secret nonce-value))
+  ;; (-> (make-sha512-hmac shared-secret nonce-value)
+  ;;     (.getBytes)
+  ;;     r/base64-encode))
 
-(defn get-http-authorization-header [[{:keys [api-key
-                                              shared-secret
-                                              key-type
-                                              encoding-type
-                                              url
-                                              request-body
-                                              actor
-                                              content-type
-                                              http-method
-                                              actor-in-hash
-                                              current-timestamp]
-                                       :or { ;; Grants or humanities keys? We'll start with grants. 
-                                            api-key "Ovo1FxW0yAz7-HNbdnM9"
-                                            shared-secret "hziQkSgRpdZla_giFfCK4h_OT98ykZM4ZYfoERvB"
-                                            key-type "API"
-                                            encoding-type "Nonce"
-                                            url (service-urls :records) ;; add something to it? 
-                                            request-body ""
-                                            actor "torysa" ;; netid
-                                            content-type "application/json" 
-                                            http-method "GET"
-                                            actor-in-hash true
-                                            current-timestamp (.format DATE-FORMAT (new java.util.Date))}
-                                       :as var-map}]]
+(defn get-http-authorization-header
+  "Get the authorization header necessary for use of some BYU web services"
+  [& [{:keys [api-key
+              shared-secret
+              key-type
+              encoding-type
+              url
+              request-body
+              actor
+              content-type
+              http-method
+              actor-in-hash
+              current-timestamp]
+       :or { ;; Grants or humanities keys? We'll start with grants. 
+            api-key "Ovo1FxW0yAz7-HNbdnM9"
+            shared-secret "hziQkSgRpdZla_giFfCK4h_OT98ykZM4ZYfoERvB"
+            key-type "API"
+            encoding-type "Nonce"
+            url (service-urls :records)
+            request-body ""
+            actor "torysa" ;; netid
+            content-type "application/json" 
+            http-method "GET"
+            actor-in-hash true
+            current-timestamp (.format DATE-FORMAT (new java.util.Date))}
+       :as var-map}]]
   (if (VALID-KEY-TYPES key-type)
     (case encoding-type
       "URL" (let [actor_value (when-not (empty? actor) (str "," actor))
                   encoded-url (url-encode var-map)]
               (str encoding-type "-Encoded-" key-type "-Key " api-key "," encoded-url))
-      "NONCE" (let [nonce-obj (get-nonce api-key actor)
+      "Nonce" (let [nonce-obj (get-nonce api-key actor)
                     encoded-url (nonce-encode shared-secret (get nonce-obj "nonceValue"))
                     nonce-key (get nonce-obj "nonceKey")]
                 (str "Nonce-Encoded-" key-type "-Key " api-key "," nonce-key "," encoded-url)))
     (throw (Exception. (str "Invalid key-type " key-type)))))
+
+
+
+(defn get-student-data [&[{:keys [service param netid]
+                           :or {service :records
+                                param "081270232" ;; TODO, IS NOT THE BYU ID#. get from person info (cas login?)
+                                netid "torysa" ;; TODO
+                                }}]]
+  (let [service-url (str (SERVICE-URLS service) param) ;; year necessary?
+        auth-header (get-http-authorization-header {; :api-key ;TODO get from env?
+                                        ; :shared-secret ;TODO get from env?
+                                                    :key-type "API" ;; Unnecessary
+                                                    :encoding-type "Nonce"
+                                                    :url service-url
+                                                    :request-body ""
+                                        ; :actor "torysa" ;; TODO from CAS
+                                                    :content-type "application/json"
+                                                    :http-method "GET"
+                                                    :actor-in-hash true
+                                                    })]
+    (client/get service-url {:headers {:authorization auth-header}})))
