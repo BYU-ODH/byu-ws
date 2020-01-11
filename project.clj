b971fa782d81c781708e9bc96201e203a835967d
Tory S. Anderson <torys.anderson@gmail.com>
2020-01-10 21:08:39 -0700

    v1: upgrades

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject byu-odh/byu-ws "0.1.1"
+(defproject byu-odh/byu-ws "1"
   :description "BYU Web Service requests"
   :url "https://github.com/BYU-ODH/byu-ws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.1.0"]
-                 [cheshire "5.6.1"]
-                 [buddy "1.0.0"]
-                 [io.jsonwebtoken/jjwt "0.6.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.10.0"]
+                 [cheshire "5.9.0"]
+                 [buddy "2.0.0"]
+                 [io.jsonwebtoken/jjwt "0.9.1"]])
